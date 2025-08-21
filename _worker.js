10b771bfa453102f1c29bcaa100a3ab710ea0653
Adel Hashemi <110264820+Adel-Hashemi@users.noreply.github.com>
2025-08-22 02:23:40 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -1525,12 +1525,12 @@ async function 生成配置信息(userID, hostName, sub, UA, RproxyIP, _url, fak
             if (hostName.includes("worker") || hostName.includes("notls")) {
                 const randomPorts = httpPorts.concat('80');
                 addressesnotls = addressesnotls.concat(
-                    cfips.map(cidr => generateRandomIPFromCIDR(cidr) + ':' + randomPorts[Math.floor(Math.random() * randomPorts.length)] + '#CF随机节点' + String(counter++).padStart(2, '0'))
+                    cfips.map(cidr => generateRandomIPFromCIDR(cidr) + ':' + randomPorts[Math.floor(Math.random() * randomPorts.length)] + '👻' + String(counter++).padStart(2, '0'))
                 );
             } else {
                 const randomPorts = httpsPorts.concat('443');
                 addresses = addresses.concat(
-                    cfips.map(cidr => generateRandomIPFromCIDR(cidr) + ':' + randomPorts[Math.floor(Math.random() * randomPorts.length)] + '#CF随机节点' + String(counter++).padStart(2, '0'))
+                    cfips.map(cidr => generateRandomIPFromCIDR(cidr) + ':' + randomPorts[Math.floor(Math.random() * randomPorts.length)] + '👻' + String(counter++).padStart(2, '0'))
                 );
             }
         }
@@ -4568,3 +4568,4 @@ async function nginx() {
 	`
     return text;
 }
+
